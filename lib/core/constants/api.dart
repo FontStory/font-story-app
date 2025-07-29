0593be56a94313ef8584675f3f9321311ae8afd8
Aria Ramin <ariaramin24@gmail.com>
2025-07-29 12:46:23 +0330

    Initial commit

---
+++ lib/core/constants/api.dart
@@ -0,0 +1,4 @@
+const String baseUrl =
+    'https://raw.githubusercontent.com/FontStory/font-story-data/main/data';
+const String fontsUrl = '/fonts.json';
+const String stylesUrl = '/styles.json';
